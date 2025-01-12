c664cc7cbba7072713ff3e07abb7efec1cf0f487
CristianoRDO <crisrooliveira.2004@gmail.com>
2025-01-12 20:22:19 -0300

    Corrigindo Comentário Sobre Função de Login

--- app/src/main/java/br/edu/ifsp/dmo1/logindatastore/ui/main/MainViewModel.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/logindatastore/ui/main/MainViewModel.kt
@@ -21,18 +21,6 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val _loggedIn = MutableLiveData<Boolean>()
     val loggedIn: LiveData<Boolean> = _loggedIn
 
-    fun login(email: String, passwd: Long, saveLogin: Boolean, stayLoggedIn: Boolean) {
-        if (User.autenticate(email, passwd)) {
-            _loggedIn.value = true
-            if (saveLogin || stayLoggedIn)
-                savePreferences(email, passwd, saveLogin, stayLoggedIn)
-            else
-                savePreferences("", 0L, saveLogin, stayLoggedIn)
-        } else {
-            _loggedIn.value = false
-        }
-    }
-
     /*
      * Esta função realiza o login do usuário, autenticando o email e a senha.
      * Também salva as preferências do usuário com base nos parâmetros fornecidos.
@@ -42,22 +30,21 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
      * mas marque a opção de "stayLoggedIn", as credenciais permanecerão salvas
      * mesmo após o logout, o que pode gerar inconsistências.
      *
-     * Por isso, criei uma função alternativa que elimina esse comportamento.
      * No entanto, como não tinha certeza se poderia realizar essa alteração,
      * mantive a versão original (implementada no roteiro).
      */
 
-    /*fun login(email: String, passwd: Long, saveLogin: Boolean, stayLoggedIn: Boolean) {
+    fun login(email: String, passwd: Long, saveLogin: Boolean, stayLoggedIn: Boolean) {
         if (User.autenticate(email, passwd)) {
             _loggedIn.value = true
-            if (saveLogin)
+            if (saveLogin || stayLoggedIn) // if(saveLogin) -> Apenas isso.
                 savePreferences(email, passwd, saveLogin, stayLoggedIn)
             else
                 savePreferences("", 0L, saveLogin, stayLoggedIn)
         } else {
             _loggedIn.value = false
         }
-    }*/
+    }
 
     private fun savePreferences(email: String, password: Long, saveLogin: Boolean, stayLoggedIn: Boolean) {
         viewModelScope.launch {
